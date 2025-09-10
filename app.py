c860da10cb4f8ae337ce8e09883f91856950be6e
HeySiriSirwan <sirwanamini04@gmai.com>
2025-09-10 17:08:22 +0330

    change flask port to 9000

--- app.py
+++ app.py
@@ -152,6 +152,6 @@ def metrics():
 if __name__ == "__main__":
     app.run(
         host=os.getenv("FLASK_BIND_HOST", "0.0.0.0"),
-        port=int(os.getenv("FLASK_PORT", 5000)),
+        port=int(os.getenv("FLASK_PORT", 9000)),
         debug=True,
     )
